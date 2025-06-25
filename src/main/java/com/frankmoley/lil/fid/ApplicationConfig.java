e5734a24a8f0b4832a46e9258307f14c21a9fc9f
Gajendra Honnaiah <gajendraph@gmail.com>
2025-06-25 01:27:44 -0500

    App config for profiles

--- src/main/java/com/frankmoley/lil/fid/ApplicationConfig.java
+++ src/main/java/com/frankmoley/lil/fid/ApplicationConfig.java
@@ -5,6 +5,7 @@ import com.frankmoley.lil.fid.service.OutputService;
 import com.frankmoley.lil.fid.service.TimeService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 
 @PropertySource("classpath:application.properties")
@@ -17,8 +18,15 @@ public class ApplicationConfig {
   private String name;
 
   @Bean
+  @Profile("!dev")
   public TimeService timeService(){
     return new TimeService(true);
+  }
+
+  @Bean
+  @Profile("dev")
+  public TimeService timeService12(){
+    return new TimeService(false);
   }
 
   @Bean
