435ea9346be85a8bb5ea85d723c9eedd3eef9988
Gajendra Honnaiah <gajendraph@gmail.com>
2025-06-25 15:03:16 -0500

    component scanning

--- src/main/java/com/frankmoley/lil/fid/ApplicationConfig.java
+++ src/main/java/com/frankmoley/lil/fid/ApplicationConfig.java
@@ -1,37 +1,10 @@
 package com.frankmoley.lil.fid;
 
-import com.frankmoley.lil.fid.service.GreetingService;
-import com.frankmoley.lil.fid.service.OutputService;
-import com.frankmoley.lil.fid.service.TimeService;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 
+@ComponentScan(basePackages = "com.frankmoley.lil.fid")
 @PropertySource("classpath:application.properties")
 public class ApplicationConfig {
-
-  @Value("${app.greeting}")
-  private String greeting;
-
-  @Value("${app.name}")
-  private String name;
-
-  @Value("#{new Boolean(environment['spring.profiles.active']!='dev')}")
-  private boolean is24;
-
-  @Bean
-  public TimeService timeService(){
-    return new TimeService(is24);
-  }
-
-  @Bean
-  public OutputService outputService(GreetingService greetingService, TimeService timeService){
-    return new OutputService(greetingService, timeService, name);
-  }
-
-  @Bean
-  public GreetingService greetingService(){
-    return new GreetingService(greeting);
-  }
 
 }
